a715b22bd18c22ddafec3371495d6a69e01bbcd1
Kamil <digir47@gmail.com>
2022-01-06 16:52:27 +0100

    Push

--- app/src/main/java/com/digirr/fitdiet/activities/AddProductActivity.kt
+++ app/src/main/java/com/digirr/fitdiet/activities/AddProductActivity.kt
@@ -19,21 +19,5 @@ class AddProductActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
         setContentView(R.layout.activity_add_product)
-
-//        val navController = findNavController(R.id.addProductNavHost)
-//
-//        AppBarConfiguration(setOf(R.id.productFinderFragment, R.id.addItemFragment))
-//
-//        //setupActionBarWithNavController(navController)
-//
-//        //Przejscie, mozna dodac do onClick
-//        navController.navigate(AddItemFragmentDirections.actionAddItemFragmentToProductFinderFragment().actionId)
-
-//        addItemButton.setOnClickListener {
-//            Toast.makeText(applicationContext, "Siema", Toast.LENGTH_LONG).show()
-//            addItemButton.text = "Zmienia sie"
-//            findNavController().navigate(ProductFinderFragmentDirections.actionProductFinderFragmentToAddItemFragment().actionId)
-//        }
-
     }
 }
